92789482767778e0cfc1d2c78016a315cf647bfa
Nucizz <calvin.suciawan@binus.ac.id>
2023-12-10 14:00:55 +0700

    Remove FB Config Test

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,9 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getToken } from "firebase/messaging";
 import { getMessaging } from 'firebase/messaging/sw'
 
-const FIREBASE_CONFIG = 
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBT3MqzkEPsuf--4dTW4yj2K2QJPWksJ6E",
+  authDomain: "beefood-binusian.firebaseapp.com",
+  projectId: "beefood-binusian",
+  storageBucket: "beefood-binusian.appspot.com",
+  messagingSenderId: "450573117232",
+  appId: "1:450573117232:web:e47887b17c4e02ef37caf5",
+  measurementId: "G-1RPXWNBCE8"
+}
 
-const MESSAGING_KEY = 
+const MESSAGING_KEY = "BN_jQ2yOdiqIMZSGe5-SBg8EKY7aiunu5bUG_NEc4Ky1B6Kdgt-DdxsBZi62oyXnphRIgeAjmTzlQcC_TEdVbWc"
 
 export const app = initializeApp(FIREBASE_CONFIG)
 
