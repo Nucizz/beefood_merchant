2abb191cadb9f7e8c7838cdc8d33e558344005c1
Nucizz <calvinanacia123@gmail.com>
2024-06-17 00:22:48 +0700

    Updated meta desc

---
+++ public/publicEnv.js
@@ -0,0 +1,12 @@
+
+ const process = {
+          env: {
+            REACT_APP_FIREBASE_CONFIG_API_KEY: 'AIzaSyBT3MqzkEPsuf--4dTW4yj2K2QJPWksJ6E',
+            REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN: 'beefood-binusian.firebaseapp.com',
+            REACT_APP_FIREBASE_PROJECT_ID: 'beefood-binusian',
+            REACT_APP_FIREBASE_CONFIG_PROJECT_ID: 'beefood-binusian.appspot.com',
+            REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID: '450573117232',
+            REACT_APP_FIREBASE_CONFIG_APP_ID: '1:450573117232:web:e47887b17c4e02ef37caf5',
+            REACT_APP_FIREBASE_CONFIG_MEASUREMENT_ID: 'G-1RPXWNBCE8'
+  }
+ }
